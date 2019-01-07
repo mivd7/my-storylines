97132029d9e900f6fdaaf312d4f3fe469b96f9af
MIVD <mi.vandeurzen@gmail.com>
2019-01-07 16:32:25 +0100

    generate opening lines from gql-server

--- src/components/startStory.js
+++ src/components/startStory.js
@@ -1,25 +1,45 @@
 import React, { Component } from 'react'
 import {Link} from 'react-router-dom'
-import { threeLines } from '../lib/openingLines'
+import {getRandomLines} from '../lib/openingLines'
 
-export default class startStory extends Component {
-  handleClick(){
-    window.location.reload()
+import gql from "graphql-tag";
+import { Query } from "react-apollo";
+
+const GET_STORIES = gql`
+  {
+    allStories {
+      id
+      content
+    }
   }
+`;
 
-  render() {
-    return( 
-      <div>
-        <h2>CHOOSE AN OPENING LINE</h2>
+const startStory = () => (
+  <Query query={GET_STORIES}>
+    {({ loading, error, data }) => {
+      console.log(data)
+      if (loading) return "Loading...";
+      if (error) return `Error! ${error.message}`;
+
+      const stories = data.allStories
+      const mapStories = stories.map(story => story.content)
+      console.log(mapStories)
+      const threeLines = getRandomLines(mapStories, 3)
+      console.log(threeLines)
+
+      return (
+        <div>
+          <h2>CHOOSE AN OPENING LINE</h2>
           <iframe title="7dwarfs" src="https://giphy.com/embed/AOqKdtVvmMAI8" width="360" height="200" frameBorder="0" class="giphy-embed" allowFullScreen></iframe>
-        <p>{threeLines[0]}</p>
+          <p>1. {threeLines[0]}</p>
           <br/>
-        <p>{threeLines[1]}</p>
-          <br/>
-        <p>{threeLines[2]}</p>
-          <br/>
-        <button onClick={this.handleClick}>I don't like these, gimme new ones!</button>
-        <Link to={`/`}><button>Take me back</button></Link>
-      </div>)
-  }
-}
+          <p>2. {threeLines[1]}</p>
+          <br />
+          <p>3. {threeLines[2]}</p>
+        </div>
+      )
+    }}
+  </Query>
+);
+
+export default startStory
