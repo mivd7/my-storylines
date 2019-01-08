a0a14e51c6f4e6a823cb876cdf4d64eee46d8d58
MIVD <mi.vandeurzen@gmail.com>
2019-01-08 14:56:15 +0100

    cleaner code

--- src/components/storyInProgress.js
+++ src/components/storyInProgress.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import { ApolloConsumer } from 'react-apollo';
+import {Link} from 'react-router-dom'
 import {STORY_QUERY} from '../actions/queries'
 
 export default class StoryInProgress extends Component {
@@ -9,13 +10,14 @@ export default class StoryInProgress extends Component {
 
   render() {
     console.log(this.state)
-    console.log(this.props)
     const storyId = this.props.match.params.id
     return (
       <ApolloConsumer>
         {client => (
           <div>
-            {this.state.story && <p>{this.state.story.content}</p>}
+            {this.state.story === null && <h1>Once upon a time...</h1>}
+            {this.state.story && <div><h1>Once upon a time...</h1>
+                                      <p>{this.state.story.content}</p></div>}
             <button
               onClick={async () => {
                 const { data } = await client.query({
@@ -25,8 +27,11 @@ export default class StoryInProgress extends Component {
                 this.storyFetched(data.story);
               }}
             >
-              Click here to start!
+              Click to start the story!
             </button>
+            <br/>
+            <Link to={'/'}><p>Go back home</p></Link>
+            <Link to={'/start'}><p>Pick a different opening line</p></Link>
           </div>
         )}
       </ApolloConsumer>
