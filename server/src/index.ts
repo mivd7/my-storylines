f3091609785d16100443747a5fa042347a144b62
MIVD <mi.vandeurzen@gmail.com>
2019-02-21 23:07:58 +0100

    dotenv/config via ts-node

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,10 +1,6 @@
 import { GraphQLServer } from 'graphql-yoga'
 import resolvers from './resolvers'
 import { prisma } from '../generated/prisma-client';
-import dotenv from 'dotenv'
-
-console.log(dotenv)
-console.log(process.env)
 
 const server = new GraphQLServer({
   typeDefs: './schema.graphql',
